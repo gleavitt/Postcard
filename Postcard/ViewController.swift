4e3c60368db5181d41126427f62d4583c1bfe6eb
Geoff Leavitt <gleavitt@mac.com>
2015-03-05 22:57:42 -0800

    Second Revision

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController: UIViewController
 {
     
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
@@ -31,16 +32,26 @@ class ViewController: UIViewController
     
     @IBAction func sendMailButtonPressed(sender: UIButton)
     {
+        //revealing the name label
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        
         //revealing the message label area
         messageLabel.hidden = false
         messageLabel.text = enterMessageTextField.text
         messageLabel.textColor = UIColor.redColor()
         
+        //clearing the text in name box and releasing keyboard
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
         //clearing the text in message box and releasing keyboard
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
         
+        //changing the appearance of the sent button
         mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        mailButton.backgroundColor = UIColor.orangeColor()
     }
 
 }
